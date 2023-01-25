6cae84362f345e5f9bf6349fe8b54e3890f2bb49
flashback220 <rayhidayat220@gmail.com>
2023-01-26 04:23:41 +0700

    setup slices and github API

--- redux/slices/githubReposSlice.ts
+++ redux/slices/githubReposSlice.ts
@@ -1,14 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
+
 
 export interface GithubRepos {
-  repos: [];
+  repos: [] | Repos[];
   user: string;
+  isLoading: Boolean;
+  error: null | unknown;
+  profile: {};
 }
+
+interface Repos {
+  id:number
+  full_name:string
+}
+
+export const fetchRepositories = createAsyncThunk(
+  "fetchRepos/repositories",
+  async (user:string, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(
+        `${
+          process.env.NEXT_PUBLIC_API_GITHUB_USERS as string
+        }/${user}/repos?per_page=50`
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const fetchProfileUser = createAsyncThunk(
+  "fetchRepos/profileUser",
+  async (user: string, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_GITHUB_USERS as string}/${user}`
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 const initialState: GithubRepos = {
   repos: [],
-  user: "ray",
+  user: "",
+  isLoading: false,
+  error: null,
+  profile: {},
 };
 
 export const githubReposSlice = createSlice({
@@ -16,10 +58,39 @@ export const githubReposSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    
+    builder.addCase(fetchRepositories.pending, (state, action) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(fetchRepositories.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.repos = action.payload;
+      state.error = null;
+    });
+
+    builder.addCase(fetchRepositories.rejected, (state, action) => {
+      state.isLoading = false;
+      state.repos = [];
+      state.error = action.payload;
+    });
+
+    builder.addCase(fetchProfileUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(fetchProfileUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.error = null;
+      state.profile = action.payload;
+    });
+    builder.addCase(fetchProfileUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.user = "";
+      state.error = action.payload;
+    });
   },
 });
 
-export const {} = githubReposSlice.actions;
+// export const {} = githubReposSlice.actions;
 
 export default githubReposSlice.reducer;
--- redux/store.ts
+++ redux/store.ts
@@ -1,14 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import githubRepos from "../redux/slices/githubReposSlice";
-
+import thunkMiddleware from "redux-thunk";
 
 export const store = configureStore({
   reducer: {
-    data: githubRepos,
+    githubRepos: githubRepos,
   },
+  middleware: [thunkMiddleware],
 });
+
+
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
